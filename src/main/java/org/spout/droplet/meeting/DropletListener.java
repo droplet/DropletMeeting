3c6016d4dcae50dc52e6d970e3e8091b84f48149
Wulfspider <hello@wulfspider.com>
2013-06-13 22:11:56 -0400

    Update pom.xml, properties.yml, and HEADER.txt with changes. Update license headers in source. Add basic Travis-CI config file.

--- src/main/java/org/spout/droplet/meeting/DropletListener.java
+++ src/main/java/org/spout/droplet/meeting/DropletListener.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of DropletMeeting.
  *
- * Copyright (c) 2012, Spout LLC <http://www.spout.org/>
+ * Copyright (c) 2012 Spout LLC <http://www.spout.org/>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
